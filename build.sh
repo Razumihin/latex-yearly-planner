004209ef8d84bb43bf44ba92e845d882797e4f03
Sami Ketola <sami.ketola@gmail.com>
2024-11-29 11:46:05 +0200

    * Added defaults for onyx boox go 10.3 and changed the build defaults to point to that

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ else
 fi
 
 if [ -z "$CONFIG_FILES" ]; then
-  CONFIG_FILES='cfg/base.yaml,cfg/template_months_on_side.yaml,cfg/sn_a5x.mos.default.yaml,cfg/sn_a5x.mos.default.dailycal.yaml'
+  CONFIG_FILES='cfg/base.yaml,cfg/template_breadcrumb.yaml,cfg/onyx_103.breadcrumb.default.yaml,cfg/onyx_103.breadcrumb.default.dailycal.yaml'
 fi
 
 NAME="planner.${TARGET_YEAR}"
